58f09dd3f346c1f9654ee2c4adcfff80d22be9a3
Djordje Pantelic <pantelicdjole94@gmail.com>
2019-01-13 14:13:53 +0100

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -86,7 +86,7 @@ opt = keras.optimizers.Adam(lr=0.0001)
 model.compile(loss = 'categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 model.fit_generator(trening_gomila, steps_per_epoch = 280, validation_data = validaciona_gomila,
-                    validation_steps = 30, epochs = 100, verbose = 2)
+                    validation_steps = 30, epochs = 120, verbose = 2)
 
 
 model.save('melanoma.h5')
